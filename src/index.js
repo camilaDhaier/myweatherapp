ed2ee2dc340caff7549fe23e09ba653c4a32018e
camilaDhaier <violincamila@gmail.com>
2024-07-17 17:42:52 -0300

    search functionality using Api

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
-function handleSearchSubmit(event){
-    event.preventDefault();
-    let searchInput = document.querySelector("#search-form-input");
-    let cityElement = document .querySelector("#city");
-    cityElement .innerHTML= searchInput.value;
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "3d1a7f2f23o09983b2643c5a0t1032fc";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
-searchFormElement .addEventListener("submit", handleSearchSubmit);
+searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Paris");

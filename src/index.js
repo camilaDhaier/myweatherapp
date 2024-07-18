8f6ea53cd85686b27cee1746c46c2c9636882ebd
camilaDhaier <violincamila@gmail.com>
2024-07-18 11:03:47 -0300

    injected the temperature icons

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ function refreshWeather(response) {
   let windSpeedElement = document.querySelector("#wind-speed");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
 
 
   cityElement.innerHTML = response.data.city;
@@ -15,6 +16,7 @@ function refreshWeather(response) {
   humudityElement .innerHTML = `${response .data .temperature .humidity}%`;
   windSpeedElement .innerHTML = `${response .data .wind .speed}km/h` ;
   temperatureElement.innerHTML = Math.round(temperature);
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
 }
 
 function formatDate (date){
@@ -31,8 +33,10 @@ function formatDate (date){
     ];
     let day = days[date .getDay()];
 
-    if minutes = `0${minutes}`;
-    
+    if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
     return `${day} ${hours}:${minutes}`;
 }
 
